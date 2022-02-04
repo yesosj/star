28442935491ae0b292e96be792ee40138ed52d93
yesosj <yesosj@naver.com>
2022-02-04 11:09:26 +0900

    First commit

---
+++ src/year.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Year(){
+    return(
+        <div className="Year">
+            <div className="MouseImage">
+            <h3 style={{color: 'black', fontSize: '40px', fontStyle: 'italic'}}><h3 style={{color: 'black' , fontSize: '20px', fontStyle: 'italic'}}>제 1</h3> 쥐띠</h3>
+            </div>
+        </div>
+    )
+}
+
+
+export default Year;
